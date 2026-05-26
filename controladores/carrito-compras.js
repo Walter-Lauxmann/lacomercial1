db00ec8a3b9f39d5f3d52cc61a2c49dac430c03b
Walter Lauxmann <walterlauxmann@institutocedec.com>
2026-05-26 14:08:18 -0300

    Carrito compras interactivo

--- controladores/carrito-compras.js
+++ controladores/carrito-compras.js
@@ -1,3 +1,55 @@
+function agregar(boton) {
+    const articulo = boton.closest('.servicio');
+
+    const codigo = articulo.querySelector('[name=codigo').textContent;
+    const nombre = articulo.querySelector('[name=nombre').textContent;
+    const precio = articulo.querySelector('[name=precio').textContent;
+
+    const inputCantidadExistente = document.getElementById(`cantidad_${codigo}`);
+
+    if(inputCantidadExistente) {
+        let cantidadActual = parseInt(inputCantidadExistente.value);
+        inputCantidadExistente.value = cantidadActual + 1;
+
+        calcular();
+
+        return;
+    }
+
+    const tabla = document.getElementById('lista-tabla');
+    const nuevaFila = document.createElement('tr');
+
+    nuevaFila.innerHTML = `
+        <td data-label="Código">${codigo}</td>
+        <td data-label="Nombre">${nombre}</td>
+        <td data-label="Cantidad">
+            <input 
+                type="number" 
+                name="cantidad" 
+                id="cantidad_${codigo}"
+                value="1"
+                min="0"
+                onchange="calcular()"
+                />
+        </td>
+        <td data-label="Precio">$ <span name="precio" id="precio1">${precio}</span>.- </td>
+        <td data-label="Importe">$ <span name="importe" id="importe_${codigo}">${precio}</span>.-</td>
+        <td><button class="boton-eliminar" onclick="eliminar(this)">X</button></td>
+    `;
+
+    tabla.appendChild(nuevaFila);
+
+    calcular()
+}
+
+function eliminar(boton) {
+    const fila = boton.closest('tr');
+    fila.remove();
+    calcular();
+}
+
+
+
 function calcular() {
     /* Elementos DOM */
     const cantidades = document.getElementsByName('cantidad');
